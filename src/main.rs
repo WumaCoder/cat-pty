5319571042619914f0d42bd7698855c34e0a43f1
WumaCoder <1829913225@qq.com>
2023-12-10 14:53:57 +0800

    test: 初步完成pty

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,80 @@
-use nix::{
-    libc,
-    sys::wait::{waitpid, WaitStatus},
-    unistd::{close, dup2, execvp, fork, pipe, read, ForkResult},
-};
-use std::{ffi::CString, io::Write, os::unix::io::AsRawFd};
+use nix::fcntl::{open, OFlag};
+use nix::pty::{grantpt, posix_openpt, ptsname, unlockpt};
+use nix::sys::stat::Mode;
+use nix::unistd::{close, dup, dup2, setsid, write};
+use std::{ffi::CString, os::fd::AsRawFd, thread, time::Duration};
 
-const BUFFER_SIZE: usize = 4096;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 打开伪终端主设备
+    let master_fd = posix_openpt(OFlag::O_RDWR)?;
 
-fn main() {
-    let (input_pipe_read, input_pipe_write) = pipe().expect("创建输入管道失败");
-    let (output_pipe_read, output_pipe_write) = pipe().expect("创建输出管道失败");
+    // 将主设备授予子进程
+    grantpt(&master_fd)?;
 
-    println!("hello main");
-    match unsafe { fork() } {
-        Ok(ForkResult::Parent { child }) => {
-            // Parent process
-            println!("main process");
+    // 解锁从设备
+    unlockpt(&master_fd)?;
 
-            // 关闭不必要的管道端口
-            close(input_pipe_write).expect("关闭输入管道写端失败");
-            close(output_pipe_write).expect("关闭输出管道写端失败");
+    // 获取从设备名称
+    let slave_name = CString::new(unsafe { ptsname(&master_fd) }?.as_str())?;
 
-            // 读取输出
-            let mut buffer = [0u8; BUFFER_SIZE];
-            loop {
-                match read(output_pipe_read, &mut buffer) {
-                    Ok(0) => break, // EOF
-                    Ok(n) => {
-                        let output = String::from_utf8_lossy(&buffer[..n]);
-                        print!(">>{}", output);
-                    }
-                    Err(e) => {
-                        eprintln!("从输出管道读取数据时发生错误 {:?}", e);
-                        break;
+    // 创建子进程
+    match unsafe { nix::unistd::fork()? } {
+        nix::unistd::ForkResult::Parent { child } => {
+            // 关闭父进程中不需要的文件描述符
+            println!("hello main");
+
+            thread::spawn({
+                let master_fd = master_fd.as_raw_fd();
+                move || {
+                    loop {
+                        // 读取子进程的输出
+                        let mut output_data = [0u8; 1024];
+                        let len = nix::unistd::read(master_fd, &mut output_data).unwrap_or(0);
+                        if len > 0 {
+                            print!("{}", String::from_utf8_lossy(&output_data[..len]));
+                        }
                     }
                 }
-            }
+            });
+
+            write(master_fd.as_raw_fd(), "echo 'Hello, World!'".as_bytes()).unwrap();
+            write(master_fd.as_raw_fd(), "\n".as_bytes()).unwrap();
+            write(master_fd.as_raw_fd(), "zsh".as_bytes()).unwrap();
+            write(master_fd.as_raw_fd(), "\n".as_bytes()).unwrap();
+            write(master_fd.as_raw_fd(), "echo 'Hello, World!'".as_bytes()).unwrap();
+            write(master_fd.as_raw_fd(), "\n".as_bytes()).unwrap();
 
             // 等待子进程完成
-            match waitpid(child, None) {
-                Ok(WaitStatus::Exited(_, status)) => {
-                    println!("子进程退出，状态码: {}", status);
-                }
-                _ => {
-                    eprintln!("等待子进程退出时发生错误");
-                }
-            }
+            let status = nix::sys::wait::waitpid(child, None)?;
+            println!("子进程退出，状态码: {:?}", status);
         }
-        Ok(ForkResult::Child) => {
-            // Child process
-            println!("child process");
+        nix::unistd::ForkResult::Child => {
+            // 在子进程中创建新的会话并设置为控制终端
+            setsid()?;
 
-            // 关闭不必要的文件描述符
-            close(input_pipe_write).expect("关闭输入管道写端失败");
-            close(output_pipe_read).expect("关闭输出管道读端失败");
+            // 打开从设备
+            let slave_fd = open(slave_name.as_c_str(), OFlag::O_RDWR, Mode::empty())?;
 
-            // 重定向标准输入和输出
-            if let Err(err) = dup2(input_pipe_read, libc::STDIN_FILENO) {
-                eprintln!("重定向标准输入失败: {}", err);
-                unsafe { libc::exit(1) };
-            }
-            if let Err(err) = dup2(output_pipe_write, libc::STDOUT_FILENO) {
-                eprintln!("重定向标准输出失败: {}", err);
-                unsafe { libc::exit(1) };
-            }
+            // 将从设备作为子进程的标准输入、输出、错误
+            dup2(slave_fd, nix::libc::STDIN_FILENO)?;
+            dup2(slave_fd, nix::libc::STDOUT_FILENO)?;
+            dup2(slave_fd, nix::libc::STDERR_FILENO)?;
 
-            // 关闭子进程不需要的管道端口
-            close(input_pipe_read).expect("关闭输入管道读端失败");
-            close(output_pipe_write).expect("关闭输出管道写端失败");
+            println!("hello child");
 
-            // 执行 Bash 命令
-            let cmd = CString::new("ping").expect("Failed to create CString");
-            let echo = CString::new("114.114.114.114").expect("Failed to create CString");
-            let args = [cmd.as_c_str(), echo.as_c_str()];
-            execvp(cmd.as_c_str(), &args).expect("Failed to execute command");
-        }
-        Err(_) => {
-            eprintln!("Fork failed");
+            // 关闭不再需要的文件描述符
+            close(slave_fd)?;
+            close(master_fd.as_raw_fd())?;
+
+            // 执行 Bash
+            let cmd = CString::new("bash")?;
+            let args = [cmd.as_c_str()];
+            nix::unistd::execvp(cmd.as_c_str(), &args)?;
+
+            // 如果 execvp 失败，退出子进程
+            unsafe { nix::libc::_exit(1) };
         }
     }
-    println!("hello main");
+
+    Ok(())
 }
